057a0c12df690e775420fe4b6eaa27b565bdb4bf
dianekapr <dianekapratiwi72@gmail.com>
2025-04-08 15:10:17 +0700

    deleting folder yg ga kepake

--- cms-site/src/app/api/jobs/route.ts
+++
@@ -1,9 +0,0 @@
-import jobModel from "@/db/models/jobmodel";
-
-export async function GET (){
-    const jobs = await jobModel.getAll()
-
-    console.log(jobs,'ini jobs dari mongo db<<');
-    
-    return Response.json(jobs)
-}
