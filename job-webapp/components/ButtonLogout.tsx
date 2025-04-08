28f6ffd090c07e7c05e2c51991cdf18f24a6ab15
dianekapr <dianekapratiwi72@gmail.com>
2025-04-08 15:08:41 +0700

    userJob

--- job-webapp/components/ButtonLogout.tsx
+++ job-webapp/components/ButtonLogout.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { handleLogout } from "@/app/action";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
 export default function ButtonLogout() {
+  const router = useRouter();
+
   return (
     <Button
       onClick={async () => {
         await handleLogout();
-        Router.push("/");
+        router.push("/"); // redirect langsung ke halaman home
       }}
       variant="outline"
       className="bg-secondary text-primary-foreground hover:bg-secondary/90"
